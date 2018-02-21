636db689222153918d4181cf4d266568f72ad428
siddiqabu <35294835+siddiqabu@users.noreply.github.com>
2018-02-21 15:43:36 +0530

    Create case.c

---
+++ case.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main() 
+
+{
+
+	char a[100];
+
+	int i,n,count=0,countt=0,count1=0;
+
+	scanf("%s",a);
+
+	n=strlen(a);
+
+	for(i=0;i<n;i++)
+
+	{
+
+	    if((a[i]>='a')&&(a[i]<='z')||(a[i]>='A')&&(a[i]<='Z'))
+
+	    {
+
+	       count=1;
+
+	    }
+
+	    else if((a[i]>=0)&&(a[i]>=9))
+
+	    {
+
+	        countt=1;
+
+	    }
+
+	    else
+
+	    {
+
+	        count1=1;
+
+	    }
+
+	}
+
+	if((count==1)&&(countt==1))
+
+	{
+
+	    printf("yes");
+
+	}
+
+	else
+
+	{
+
+	    printf("no");
+
+	}
+
+	return 0;
+
+}
